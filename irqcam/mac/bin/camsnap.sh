99c4bf5e9f4be10e84bdfcf91a168e9a1c2ddf35
Cliff Flood <cliph@tertimi.home.cli.ph>
2012-10-26 00:05:46 -0400

    irqcam scripts

---
+++ irqcam/mac/bin/camsnap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ../../config
+
+while true; do
+    date=`date +%d%m%y%H%M%S`
+	datefile=$name_$date.jpg
+	namefile=$name.jpg
+	echo -n "Capturing image ... "
+	$macbin $archive/$datefile
+	echo "Done."
+
+	echo -n "Making image link ... "
+	ln -fs $archive/$datefile $outdir/$namefile
+	echo "Done."
+
+	echo
+
+	sleep $interval
+
+done
+
