eb7381bc966f0c631208067a26eb6b0e3f062cee
Cliff Flood <cliph@cli.ph>
2012-11-06 23:01:35 -0500

    Resolve twitter bug

--- irqhashtag/twitterstream.sh
+++ irqhashtag/twitterstream.sh
@@ -15,8 +15,15 @@ twitter_url=https://stream.twitter.com/1/statuses/filter.json
 #	hashtag=$1
 # fi
 
-curl -s $twitter_url -d track=\'$hashtags\' -u $twitter_user:$twitter_pass 
-# | tr , '\n' | grep media_url\" | sed 's/\\//g' | cut -d\" -f4 
+
+# echo curl -s $twitter_url -d track=\'$hashtags\' -u $twitter_user:$twitter_pass 
+# echo "curl -Ns $twitter_url -d track=\'$hashtags\' -u $twitter_user:$twitter_pass | tr -u , '\n' | grep --line-buffered media_url\" | sed -l 's/\\//g'" 
+curl -Ns $twitter_url -d track=${hashtags} -u $twitter_user:$twitter_pass | tr -u , '\n' | grep --line-buffered media_url\" | sed -l 's/\\//g' | cut -d\" -f4
+# curl -v -o - $twitter_url -d track=\'$hashtags\' -u $twitter_user:$twitter_pass 2>1
+
+# | tr -u , '\n' | grep --line-buffered media_url\" # | sed -l 's/\\//g' 
+
+# | cut -d\" -f4 
 # curl $twitter_url -d track=\'$hashtags\' -u $twitter_user:$twitter_pass | tr , '\n' | grep media_url\" | sed 's/\\//g' | cut -d\" -f4 
 # curl -s $twitter_url -d track=\'$hashtags\' -u $twitter_user:$twitter_pass | tr , '\n' | grep media_url\" | sed 's/\\//g' | cut -d\" -f4 
 # cut -d\" -f4 | \
