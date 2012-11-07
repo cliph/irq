5fde9b4e4f7e6c6dc51988320859ea04e9f39920
Cliff Flood <cliph@cli.ph>
2012-11-07 00:30:54 -0500

    try a different reload

--- irqhashtag/index.php
+++ irqhashtag/index.php
@@ -10,12 +10,29 @@
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script> 
 <script> 
-var auto_refresh = setInterval(
-function()
-{
+// var auto_refresh = setInterval(
+// function()
+// {
 // $('#loaddiv').fadeOut('slow').load("pictures.html?"+(+new Date())).fadeIn("slow");
-$('#loaddiv').load("pictures.html?"+(+new Date()));
-}, 10000);
+// $('#loaddiv').load("pictures.html?"+(+new Date()));
+// }, 10000);
+
+$(document).ready(function () {
+    var interval = 500;   //number of mili seconds between each call
+    var refresh = function() {
+        $.ajax({
+            url: "pictures.html",
+            cache: false,
+            success: function(html) {
+                $('#loaddiv').html(html);
+                setTimeout(function() {
+                    refresh();
+                }, interval);
+            }
+        });
+    };
+    refresh();
+});
 </script>
 
 <div id="loaddiv"> 
